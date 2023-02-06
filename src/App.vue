f057c26016f10eb2734f307106933c6609a7d0b6
Salymka <prokopchuk.dmytro01@gmail.com>
2023-02-06 22:36:01 +0200

    add loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <!--    <div class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">-->
-    <!--      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">-->
-    <!--        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>-->
-    <!--        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>-->
-    <!--      </svg>-->
-    <!--    </div>-->
+    <div
+        v-if="isLoading"
+        class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center">
+      <svg class="animate-spin -ml-1 mr-3 h-12 w-12 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+           viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
     <div class="container">
       <section>
         <div class="flex">
@@ -32,7 +36,11 @@
               BTC
             </span>
             </div>
-            <div class="text-sm text-red-600">This Ticker already exist</div>
+            <div
+                v-if="isExist"
+                class="text-sm text-red-600">
+              This Ticker already exist
+            </div>
 
           </div>
         </div>
@@ -157,9 +165,20 @@ export default {
       tickersList: [],
       sel: null,
       graph: [],
-      APIKEY: '32acc2845c57ae4f171f4efb78bf6bf5cb8692c1acf73e68d200589261a3254b'
+      APIKEY: '32acc2845c57ae4f171f4efb78bf6bf5cb8692c1acf73e68d200589261a3254b',
+      isLoading: true,
+      tickersInfo: {},
+
+
     }
   },
+
+  created: async function () {
+    this.tickersInfo = await fetch('https://min-api.cryptocompare.com/data/all/coinlist?summary=true').then((res) => res.json())
+    console.log(this.tickersInfo.Data);
+    this.isLoading = false
+  },
+
   methods: {
     add() {
       const currentTicker = {
@@ -208,8 +227,11 @@ export default {
       this.graph = []
     },
 
-    coinHelper(){
+    coinHelper() {
 
+    },
+    isExist(tickerName) {
+      return !!this.tickersList.find(ticker => ticker.name === tickerName)
     }
   }
 }
